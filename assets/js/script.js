3c4163eef243ba214824040e472ed3a8bebc5f9b
JuanDuran85 <duran85juan@gmail.com>
2020-08-18 20:07:06 -0400

    agregando interactividad con jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#enviarCorreo').click(function (){
+        alert("El correo fue enviado correctamente...")
+    });
+
+    $('body').on('dblclick','h4',function (e) {
+        e.preventDefault();
+        $(this).css("color",'red');
+    });
+
+    $('.card-title').click(function(){
+        $('.card-text').toggle();
+    });
 });
